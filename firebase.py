1d681f5c9cb9b4156205ab60237af217aa94e35e
Cheng Yao, Liu <avex1103@gmail.com>
2023-06-01 22:06:46 +0800

    add data field

--- firebase.py
+++ firebase.py
@@ -6,6 +6,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import datetime
+import pandas as pd
 
 # 透過金鑰使用服務帳戶
 cred = credentials.Certificate(
@@ -20,10 +21,18 @@ db = firestore.client()
 humidity = 11
 lightness = 72
 temperature = 13
-IsSoilWet = 1
+isSoilWet = 1
+currentTime = datetime.now()
 
-data = {"humidity": humidity, "lightness": lightness, "temperature": temperature, "IsSoilWet": IsSoilWet}
+data = {
+    "humidity": humidity,
+    "lightness": lightness,
+    "temperature": temperature,
+    "isSoilWet": isSoilWet,
+    # firebase 會自動補上時區資訊，因此將欄位內的時間先剪去時區的 offset
+    "time": pd.Timestamp(currentTime).replace(hour=(currentTime.hour - 8))
+}
 
 # 將 data object 加入 firebase 中
-doc_ref = db.collection("sensor").document(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+doc_ref = db.collection("sensor").document(currentTime.strftime("%Y-%m-%d %H:%M:%S"))
 doc_ref.set(data)
