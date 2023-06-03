7b09c2598a8623fdaf47b4e138e21941ba961278
Cheng Yao, Liu <avex1103@gmail.com>
2023-06-03 19:03:16 +0800

    change data source to firebase realtime database

--- web/public/index.js
+++ web/public/index.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import {
-  query,
-  orderBy,
-  limit,
-  getFirestore,
-  getDocs,
-  collection,
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+  getDatabase,
+  ref,
+  get,
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+
 // 設定 fireBase
 import * as configs from "./configs.json" assert { type: "json" };
 const firebaseConfig = configs.default.firebaseConfig;
+const realtimeDatabaseNodeName = configs.default.realtimeDatabaseNodeName;
 
-// 初始化 Firebase &  Firestore
+// 初始化 Firebase & Firestore
 initializeApp(firebaseConfig);
-const db = getFirestore();
+const db = getDatabase();
 
 // 半圓的周長
 const length = 125;
@@ -21,29 +20,27 @@ const length = 125;
 var light = 50;
 var temperature = 25;
 var humidity = 50;
-var isSoilWet = 0;
+var lastWateredTime = "";
 
 // 從 fireBase 取得資料
 async function getDataFromFirebase() {
-  // 以最新的資料為主，只搜尋第1筆
-  var q = query(collection(db, "sensor"), orderBy("time", "desc"), limit(1));
-  var response = await getDocs(q);
-  response.forEach((doc) => {
+  var response = await get(ref(db, realtimeDatabaseNodeName));
+  response.forEach((datas) => {
     // 如果有撈到資料就更新
-    if (doc.data()) {
-      var datas = doc.data();
-      console.log(datas);
-      humidity = datas.humidity;
-      temperature = datas.temperature;
-      light = datas.lightness;
-      isSoilWet = datas.isSoilWet;
+    if (datas.exists()) {
+      var data = datas.val();
+      console.log(data);
+      humidity = data.humidity;
+      temperature = data.temperature;
+      light = data.lightness;
+      lastWateredTime = data.lastWateredTime;
     }
     // 沒撈到資料就設為 0
     else {
       humidity = 0;
       temperature = 0;
       light = 0;
-      isSoilWet = 0;
+      lastWateredTime = 0;
     }
   });
 }
@@ -53,10 +50,21 @@ function updateProgressBar() {
   updateData("light", light);
   updateData("temperature", temperature);
   updateData("humidity", humidity);
+  updateData("soil", lastWateredTime);
 }
 
 // 更新半圓 progress bar 的顯示狀態
 function updateData(type, value) {
+  // 取得顯示的 label
+  var targetValue = document.getElementById(`${type}-value`);
+  if (type === "temperature") {
+    targetValue.innerHTML = `${value}°C`;
+  } else if (type === "soil") {
+    targetValue.innerHTML = `${value}`;
+    return;
+  } else {
+    targetValue.innerHTML = `${value}%`;
+  }
   // 取得目標動畫
   var targetAnimation = document.getElementById(`${type}-animation`);
   // 如果是溫度的話刻度就只有50
@@ -68,13 +76,6 @@ function updateData(type, value) {
   );
   // 重新跑過一遍動畫
   targetAnimation.beginElement();
-  // 取得顯示的 label
-  var targetValue = document.getElementById(`${type}-value`);
-  if (type === "temperature") {
-    targetValue.innerHTML = `${value}°C`;
-  } else {
-    targetValue.innerHTML = `${value}%`;
-  }
 }
 
 // 每 10 分鐘更新一次資料
