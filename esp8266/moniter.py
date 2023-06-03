bf9448f191c14749d1d4053e94a4343ddddfe554
Cheng Yao, Liu <avex1103@gmail.com>
2023-06-03 17:51:34 +0800

    add function to post data to firebase

--- esp8266/moniter.py
+++ esp8266/moniter.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 from machine import Pin
 from machine import ADC
+import json
 import time
 import ntptime
 import network
 import dht
-import mip  # 下載或驗證額外套件
+import urequests
 
 def countPercentage(num):
     return round(num*100, 2)
 
+# 取得設定檔資料
+configFile = open("./configs.json", "r")
+CONFIGS = json.loads(configFile.read())
+configFile.close()
+
 # 設定 ESP8266 連接的 wifi
 wifi = network.WLAN(network.STA_IF)
-ssid = ''
-password = ''
+ssid = CONFIGS["wifiSsid"]
+password = CONFIGS["wifiPassword"]
 wifi.active(True)
 wifi.connect(ssid, password)
 
@@ -29,6 +35,10 @@ ntptime.settime()
 # 台灣時間為 UTC+8
 TIME_OFFSET = 8 * 60 * 60
 
+# 設定 fireBase realtime database 參數
+DATABASE_URL = CONFIGS["realtimeDatabaseUrl"]
+DATANODE_NAME = CONFIGS["realtimeDatabaseNode"] + ".json"
+
 # 宣告各感測器的腳位
 LDR_PIN = 0     #A0
 SOIL_PIN = 16   #D0
@@ -39,26 +49,65 @@ soilPin = Pin(SOIL_PIN, Pin.IN)
 dhtPin = dht.DHT11(Pin(DHT11_PIN))
 ldrPin = ADC(LDR_PIN)
 
+# 初始化要上傳的資料變數
+humidity = 0
+lightness = 0
+temperature = 0
+lastWateredTime = time.gmtime(0)
+
 # 持續執行
 try:
     while(1):
         # 取得光敏電阻值及計算百分比
         # read_u16 : 0-65535
-        lightPercentage = countPercentage(ldrPin.read_u16() / 65535)
+        lightness = countPercentage(ldrPin.read_u16() / 65535)
         # 觸發偵測溫度與濕度
         dhtPin.measure()
         # 取得溫度與濕度
         humidity = dhtPin.humidity()
         temperature = dhtPin.temperature()
+        # 取得當下的時間
+        currentTime = time.localtime(time.time() + TIME_OFFSET)
         # 取得連接土壤感測器 pin 腳的的值(0 or 1)
         if(not soilPin.value()):
-            currentTime = time.localtime(time.time() + TIME_OFFSET)
             hour = currentTime[3]
             # 土壤乾燥且不是中午時段(11~2點)時才澆水
             if(hour < 11 and hour >= 14):
+                # 紀錄澆水的時間
+                lastWateredTime = currentTime
                 print("土壤乾燥，請澆水")
         else:
             print("土壤濕度足夠")
+
+        # data object 儲存 sensor 讀取到的值
+        # 格式化(YYYY-MM-DD HH:mm:SS)澆水時間，以字串型態上傳到 firebase
+        lastWateredTimeFormat = ""
+        for i in range(6):
+            # 統一為 2 位數
+            if(lastWateredTime[i] < 10):
+                lastWateredTimeFormat += "0"
+            lastWateredTimeFormat += str(lastWateredTime[i])
+            if(i < 2):
+                lastWateredTimeFormat += "-"
+            elif(i == 2):
+                lastWateredTimeFormat += " "
+            elif(i < 5):
+                lastWateredTimeFormat += ":"
+        # data object 儲存 sensor 讀取到的值
+        data = {
+            "humidity": humidity,
+            "lightness": lightness,
+            "temperature": temperature,
+            "lastWateredTime": lastWateredTimeFormat
+        }
+
+        # 將 data object 加入 firebase 中
+        response = urequests.post(DATABASE_URL + DATANODE_NAME, json=data)
+        if(response.status_code == 200):
+            print("ok")
+        else:
+            print(response.status_code)
+        response.close()
         # 每 10 秒偵測一次
         time.sleep(10)
 
